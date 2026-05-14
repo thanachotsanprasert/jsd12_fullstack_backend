dc4a7f7a7ef9dda198a0cd543534d3da6df0fb7e
weerayosong <weerayosong@outlook.com>
2026-05-14 13:24:20 +0700

    add CRUD routes for users resource

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { users } from "./fakeData/fakeUsers.js";
 
 const app = express();
+
+app.use(cors);
 app.use(express.json());
 
 app.get("/", (req, res) => {
