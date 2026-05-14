d82de259e02a8ea03697274b9acfd2e41ae828cf
weerayosong <weerayosong@outlook.com>
2026-05-14 14:35:31 +0700

    refactor soc users routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { users } from "./fakeData/fakeUsers.js";
+
+// import { users } from "./fakeData/fakeUsers.js";
+// import { apiRoutes } from "./routes/v1/index.js";
+import { router as apiRoutes } from "./routes/v1/index.js";
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -40,61 +43,7 @@ app.get("/", (req, res) => {
   </html>`);
 });
 
-app.get("/users", (req, res) => {
-    res.json(users);
-});
-
-app.post("/users", (req, res) => {
-    const { username, email } = req.body || {};
-
-    if (!username || !email) {
-        return res
-            .status(400)
-            .json({ error: "username and email are required" });
-    }
-
-    const nextID = String(
-        (users.reduce((max, u) => Math.max(max, Number(u.id)), 0) || 0) + 1,
-    );
-
-    const newUser = { id: nextID, username, email };
-    users.push(newUser);
-    return res.status(201).json(newUser);
-});
-
-app.put("/users/:id", (req, res) => {
-    const user = users.find((u) => u.id === String(req.params.id));
-
-    if (!user) {
-        return res.status(404).json({ error: "not found" });
-    }
-
-    const { username, email, password } = req.body || {};
-
-    if (!username || !email || !password) {
-        return res
-            .status(400)
-            .json({ error: "username, email and password are require!" });
-    }
-
-    user.username = username;
-    user.email = email;
-    user.password = password;
-
-    return res.status(200).json(user);
-});
-
-app.delete("/users/:id", (req, res) => {
-    const userIndex = users.findIndex((u) => u.id === String(req.params.id));
-
-    if (userIndex === -1) {
-        return res.status(404).json({ error: "not found" });
-    }
-
-    const deletedUser = users.splice(userIndex, 1)[0];
-
-    return res.status(200).json(deletedUser);
-});
+app.use("/api", apiRoutes);
 
 const PORT = 3002;
 app.listen(PORT, () => {
